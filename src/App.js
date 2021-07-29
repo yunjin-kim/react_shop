73413e7eaf3530ef090e3dc9bce4a319af13d2c6
(yunjin) <sion11290805@gmail.com>
2021-07-29 17:25:48 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar,Container,Nav } from 'react-bootstrap';
 import './App.css';
-
+import tealist from './data';
 
 
 
 function App() {
+  let [tea, setTea] = useState(tealist);
   return (
     <div className="App">
+
       <Navbar bg="dark" variant="dark">
       <Container>
       <Navbar.Brand href="#home">오설록</Navbar.Brand>
@@ -24,29 +26,28 @@ function App() {
         유기농 제주 차밭에서 자란 우리 녹차 발효차 라인입니다
       </p>
     </div>
+
     <div className="container">
       <div className="row">
-        <div className="col-md-4">
-          <img src="https://www.osulloc.com/upload/kr/ko/adminImage/CV/PI/20191213161646687QE.png?quality=80" />
-          <h4>레드파파야블랙티 20입</h4>
-          <p>18,400원</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://www.osulloc.com/upload/kr/ko/adminImage/ZM/SS/20191213160304931SR.png?quality=80" />
-          <h4>삼다연제주영귤 20입</h4>
-          <p>23,000원</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://www.osulloc.com/upload/kr/ko/adminImage/GD/EP/20200716171557754KL.png?quality=80" />
-          <h4>찬물녹차 20입</h4>
-          <p>7,600원</p>
-        </div>
+        <Product />
       </div>
     </div>
 
-    
     </div>
   );
 }
+
+function Product(){
+  let [tea, setTea] = useState(tealist);
+  return(
+    <div className="col-md-4">
+    <img src={tea[0].img} />
+    <h4>{tea[0].title}</h4>
+    <p>{tea[0].price}원</p>
+    </div>
+  )
+    
+}
+
 
 export default App;
