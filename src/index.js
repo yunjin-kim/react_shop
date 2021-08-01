32011e4e87e2b563ce5961fbf34990fc2422e114
(yunjin) <sion11290805@gmail.com>
2021-08-01 13:21:03 +0900

    sub

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {BrowerRouter} from 'react-router-dom';
+
 ReactDOM.render(
+  <BrowerRouter>
     <App />,
+  </BrowerRouter>,
   document.getElementById('root')
 );
