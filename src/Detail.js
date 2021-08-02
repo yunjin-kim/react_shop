83aa4ac180e80bf9287fe5e7149fd972598cd93b
(yunjin) <sion11290805@gmail.com>
2021-08-03 08:29:47 +0900

    sass

--- src/Detail.js
+++ src/Detail.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import { Navbar,Container,Nav } from 'react-bootstrap';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import './Detail.scss';
 
-let Boxess = styled.div`
+let Box = styled.div`
   padding: 20px
 `;
 
-let Titless = styled.p`
-  font-size: 60px;
-  color: ${props => props.colorss};
+let Title = styled.p`
+  font-size: 40px;
+  color: ${props => props.color};
 `;
 
 function Detail(props){
@@ -26,16 +27,19 @@ function Detail(props){
         <Container>
         <Navbar.Brand ><Link to="/">오설록</Link></Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link ><Link to="/detail/">녹차</Link></Nav.Link>
-          <Nav.Link >발효차</Nav.Link>
-          <Nav.Link >홍차</Nav.Link>
+          <Nav.Link ><Link to="/detail/0">홍차</Link></Nav.Link>
+          <Nav.Link><Link to="/detail/1">발효차</Link></Nav.Link>
+          <Nav.Link><Link to="/detail/2">녹차</Link></Nav.Link>
         </Nav>
         </Container>
       </Navbar>
       
-        <Boxess>
-          <Titless>녹차</Titless>
-        </Boxess>
+        <Box>
+          <Title className="red">녹차</Title>
+        </Box>
+        <div className="my_alert_yellow ">
+          <p>상품이 4개밖에 남지 않았어요!</p>
+        </div>
         <div className="col-md-6">
         <img src={findProduct.img} width='50%' />
         </div>
